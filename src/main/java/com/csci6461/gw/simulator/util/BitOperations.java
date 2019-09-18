8ee43028736d850b0e99616490949338c7245cac
marche147 <i@shiki7.me>
2019-09-18 15:20:03 -0400

    Fix: Comply to standards.

--- src/main/java/com/csci6461/gw/simulator/util/BitOperations.java
+++ src/main/java/com/csci6461/gw/simulator/util/BitOperations.java
@@ -7,32 +7,59 @@ import java.util.BitSet;
  */
 public class BitOperations {
     /**
-     * Convert a number to a BitSet
+     * Convert a number to a 16-bit BitSet
      */
     public static BitSet intToBits(int num) {
+        return intToBits(num, 16);
+    }
+
+    /**
+     * Convert a number to a BitSet
+     */
+    public static BitSet intToBits(int num, int nbits) {
         BitSet bits = new BitSet();
-        int idx = 0;
-        while(num != 0) {
+        int idx = nbits - 1;
+        while(num != 0 && idx >= 0) {
             if(num % 2 == 1) {
                 bits.set(idx);
             }
             num >>>= 1;
-            idx += 1;
+            idx -= 1;
         }
         return bits;
     }
 
     /**
-     * Convert a BitSet to a number
+     * Convert a BitSet to a 16-bit number
      */
     public static int bitsToInt(BitSet bits) {
+        return bitsToInt(bits, 16);
+    }
+
+    /**
+     * Convert a BitSet to a number
+     */
+    public static int bitsToInt(BitSet bits, int nbits) {
         int num = 0;
-        for(int i = 0; i < bits.size(); i++) {
+        for(int i = nbits - 1; i >= 0; i--) {
             if(bits.get(i)) {
-                num = num | (1 << i);
+                num = num | (1 << (nbits - (i + 1)));
             }
         }
         return num;
+    }
+
+    /**
+     * Convert a string to a BitSet
+     */
+    public static BitSet stringToBits(String s) {
+        BitSet b = new BitSet();
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '1') {
+                b.set(i);
+            }
+        }
+        return b;
     }
 }
 
--- src/main/java/com/csci6461/gw/simulator/util/Element.java
+++ src/main/java/com/csci6461/gw/simulator/util/Element.java
@@ -13,6 +13,11 @@ public class Element extends BitSet {
     private int _nbits;
 
     /**
+     * Set to true if wrap around is triggered.
+     */
+    private Boolean _wrapped;
+
+    /**
      * Class constructor
      * @param   nbits the number of bits inside the register
      */
@@ -32,39 +37,47 @@ public class Element extends BitSet {
      * Element addition: self = self + addend.
      */
     public void add(int addend) {
-        int value = wraparound(BitOperations.bitsToInt(this) + addend);
-        this.set(BitOperations.intToBits(value));
+        int value = wraparound(this.value() + addend);
+        this.set(BitOperations.intToBits(value, _nbits));
     }
 
     /**
      * Element subtraction: self = self - subtraction.
      */
     public void sub(int subtraction) {
-        int value = wraparound(BitOperations.bitsToInt(this) - subtraction);
-        this.set(BitOperations.intToBits(value));
+        int value = wraparound(this.value() - subtraction);
+        this.set(BitOperations.intToBits(value, _nbits));
     }
 
     /**
      * Element multiplication: self = self * multiplier.
      */
     public void mult(int multiplier) {
-        int value = wraparound(BitOperations.bitsToInt(this) * multiplier);
-        this.set(BitOperations.intToBits(value));
+        int value = wraparound(this.value() * multiplier);
+        this.set(BitOperations.intToBits(value, _nbits));
     }
 
     /**
      * Element division: self = self / divisor.
      */
     public void div(int divisor) {
-        int value = wraparound(BitOperations.bitsToInt(this) / divisor);
-        this.set(BitOperations.intToBits(value));
+        int value = wraparound(this.value() / divisor);
+        this.set(BitOperations.intToBits(value, _nbits));
+    }
+
+    /**
+     * Element modulo: self = self % divisor.
+     */
+    public void mod(int divisor) {
+        int value = wraparound(this.value() / divisor);
+        this.set(BitOperations.intToBits(value, _nbits));
     }
 
     /**
      * Get the integer representation of the element.
      */
     public int value() {
-        return BitOperations.bitsToInt(this);
+        return BitOperations.bitsToInt(this, _nbits);
     }
 
     /**
@@ -79,6 +92,22 @@ public class Element extends BitSet {
      * Set a element by its integer representation.
      */
     public void setByValue(int value) {
-        this.set(BitOperations.intToBits(wraparound(value)));
+        this.set(BitOperations.intToBits(wraparound(value), _nbits));
+    }
+
+    /**
+     * Convert the element to string.
+     */
+    @Override
+    public String toString() {
+        String s = "";
+        for(int i = 0; i < _nbits; i++) {
+            if(this.get(i)) {
+                s += "1";
+            } else {
+                s += "0";
+            }
+        }
+        return s;
     }
 }
